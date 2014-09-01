47a4691455b203e5c6ed4465d37c29002314cce0
Arman Garakani <arman.garakani@gmail.com>
2014-08-31 21:36:14 -0600

    tip application for code path
    following the tip video. bridge to Objective C no longer available.
    Added an extension for double value

--- tipper/ViewController.swift
+++ tipper/ViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var bill_field: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+   
+    @IBOutlet weak var tipController: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tipLabel.text = "$3.14"
+        totalLabel.text = "0.0"
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +28,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func OnEditingChanged(sender: AnyObject) {
+        println("User Changed ")
+//        var ba_int = totalLabel.text.toInt()
+        var tip_percentages = [0.15,0.18,0.20]
+        var tipPercentage = tip_percentages[tipController.selectedSegmentIndex]
+        
+        var billAmount = bill_field.text.doubleValue()
 
+        var tip = billAmount * tipPercentage00
+        var total = billAmount + tip
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+
+        tipLabel.text = String(format: " $%.2f", tip);
+        totalLabel.text = String(format: " $%.2f", total);
+
+        
+    }
+
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
